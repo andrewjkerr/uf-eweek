f319eedc370e6dc985c187c3b4a28e6c3710cb68
Andrew Kerr <andrewjkerr47@gmail.com>
2013-12-22 01:56:03 -0500

    Announcement manager is "done"
    Some additions I want to add, but it's functional!

--- announcements.php
+++ announcements.php
@@ -4,20 +4,36 @@
 		DESCRIPTION: Announcement Manager
 		USAGE: Manages announcements!
 		TO-DO:
+			- Add dates to announcements
+			- Add "announcement_history" table to database
 	*/
+	require('mysql_connect.php');
 	session_start();
 	if($_SESSION['adminlevel'] == 0 || $_SESSION['adminlevel'] == 1){
 		echo '<META HTTP-EQUIV="Refresh" Content="0; URL=auth_error.php">';
 		exit();
 	}
 	if(!empty($_POST['id-edit'])){
-		require('mysql_connect.php');
 		$query = mysqli_prepare($link, 'UPDATE announcements SET text=? WHERE id=?');
 		$query->bind_param("ss", $pEdit, $pID);
 		$pID = $_POST['id-edit'];
 		$pEdit = $_POST['announcement-edit'];
 		$query->execute();
 		$_SESSION['edit-success'] = true;
+	}
+	if(!empty($_POST['id-delete'])){
+		$query = mysqli_prepare($link, 'DELETE FROM announcements WHERE id=?');
+		$query->bind_param("s", $pID);
+		$pID = $_POST['id-delete'];
+		$query->execute();
+		$_SESSION['delete-success'] = true;
+	}
+	if(!empty($_POST['announcement-add'])){
+		$query = mysqli_prepare($link, 'INSERT INTO announcements (id, text) VALUES (NULL, ?)');
+		$query->bind_param("s", $pText);
+		$pText = $_POST['announcement-add'];
+		$query->execute();
+		$_SESSION['add-success'] = true;
 	}
 ?>
 <html>
@@ -41,6 +57,15 @@
 			display: inline;
 			cursor: hand;
 		}
+		
+		.delete{
+			display: inline;
+			cursor: hand;
+		}
+		.add{
+			display: inline;
+			cursor: hand;
+		}
 	</style>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 	<script>
@@ -49,6 +74,16 @@
 			$(".edit").click(function()
 			{
 				$(this).nextAll(".edit-form:first").slideToggle(500);
+			})
+			$(".delete-form").hide();
+			$(".delete").click(function()
+			{
+				$(this).nextAll(".delete-form:first").slideToggle(500);
+			})
+			$(".add-form").hide();
+			$(".add").click(function()
+			{
+				$(this).nextAll(".add-form:first").slideToggle(500);
 			})
 		});
 	</script>
@@ -71,12 +106,19 @@
 			<?php
 				echo '<p>Hello, ' . $_SESSION['name'] . ' and welcome to the announcement management system! (v0.1)</p>';
 				echo '<hr />';
+				echo '<div class="add"><h2>Add announcement!</h2></div>';
+				echo '<div class="add-form">
+					<form method="post" action="'. $_SERVER['$PHP_SELF'] . '">
+						<textarea cols="75" rows="5" name="announcement-add"></textarea>
+						<p><input type="submit" /></p>
+					</form>
+				</div>';
 				$url = "http://localhost/~andrewjkerr/eweek/get_announcements.php";
 				$json = file_get_contents($url);
 				$obj = json_decode($json);
 				foreach($obj as $i){
 					echo "<p>" . $i->text . "</p>";
-					echo '<p><div class="edit">[Edit]</div> | <a href="announcements.php?delete=' . $i->id . '">[Delete]</a></p>';
+					echo '<p><div class="edit">[Edit]</div> | <div class="delete">[Delete]</div></p>';
 					echo '<div class="edit-form">
 							<form method="post" action="'. $_SERVER['$PHP_SELF'] . '">
 								<textarea cols="75" rows="5" name="announcement-edit">' . $i->text . '</textarea>
@@ -84,6 +126,12 @@
 								<p><input type="submit" /></p>
 							</form>
 						</div>';
+					echo '<div class="delete-form">
+						<form method="post" action="'. $_SERVER['$PHP_SELF'] . '">
+							<input type="hidden" name="id-delete" value="' . $i->id . '" /> 
+							<p><input type="submit" value="Confirm Deletion" /></p>
+						</form>
+					</div>';
 					echo "<hr />";
 				}
 				
@@ -100,6 +148,15 @@
 			echo '<script>alert("Your announcement has been updated!");</script>';
 		}
 		unset($_SESSION['edit-success']);
+		if($_SESSION['delete-success'] && !empty($_POST['id-delete'])){
+			echo '<script>alert("That announcement has been deleted!");</script>';
+		}
+		unset($_SESSION['delete-success']);
+		if($_SESSION['add-success'] && !empty($_POST['announcement-add'])){
+			echo '<script>alert("Your announcement has been added!");</script>';
+		}
+		unset($_SESSION['add-success']);
+		
 	?>
 
 </body>
